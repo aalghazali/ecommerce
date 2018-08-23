5fd5b7244887588f03162c71336ad53b1d80f409
Abdullah Alghazali <aalghazali3@gmail.com>
2018-08-22 23:12:58 -0400

    Add files via upload

---
+++ js/library_fields.js
@@ -0,0 +1,15 @@
+"use strict";
+var fields = {
+    email: {
+        message: "Must be a valid email address.",
+        required: "Email address is required.",
+        isEmail: "Email address is invalid."
+    },
+    name: {
+        required: "First name is required."
+    },
+    phone: {
+        required: "Phone number is required."
+    },
+
+};
